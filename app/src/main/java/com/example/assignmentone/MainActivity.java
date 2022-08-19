06222e877c024b16f3f0ab0c1629a75f567406e1
Uni <jt233@students.waikato.ac.nz>
2022-08-19 21:48:16 +1200

    detail display (sans yellow)

--- app/src/main/java/com/example/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.assignmentone;
 import android.view.View;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Display(View v) {
-        Toast.makeText(this,"message",Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this,"message",Toast.LENGTH_SHORT).show();
+        ImageButton button = (ImageButton) v;
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+
     }
 }
