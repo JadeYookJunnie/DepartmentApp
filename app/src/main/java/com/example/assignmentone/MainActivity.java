ba6fddc41197326e580f119550a569db659723f5
Uni <jt233@students.waikato.ac.nz>
2022-08-19 20:33:51 +1200

    onclick test

--- app/src/main/java/com/example/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.assignmentone;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Display(View v) {
+        Toast.makeText(this,"message",Toast.LENGTH_SHORT).show();
     }
 }
