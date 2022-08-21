cc008ac5a2f66d271d32af31e6497202bde8364b
Uni <jt233@students.waikato.ac.nz>
2022-08-21 23:14:07 +1200

    final version (alternate detail layout + final comments)

--- app/src/main/java/com/example/assignmentone/DetailActivity.java
+++ app/src/main/java/com/example/assignmentone/DetailActivity.java
@@ -24,23 +24,36 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        //retreives the intent
         Intent intent = getIntent();
+        //sets the default index if no tag is passed
         int index = intent.getIntExtra("topic",-1);
 
+        //if the index (the tag given to each button in accordance to the topic in the arrays) is given
         if(index>=0) {
+            //find the display banner for image
             ImageView banner = (ImageView)findViewById(R.id.detail_banner);
+            //get the name from the array based on the tag number(index)
+            //array vale matches an image in drawables
             String img_name = getResources().getStringArray(R.array.string_array_banners)[index];
             int img_id = getResources().getIdentifier(img_name, "drawable", getPackageName());
+            //get the drawable and display in imageview
             Drawable drawable = getDrawable(img_id);
             banner.setImageDrawable(drawable);
             banner.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
+            //find the textview for title
+            //get the title from the array based on the tag number (index)
             TextView title = (TextView) findViewById(R.id.detail_title);
             String topic_name = getResources().getStringArray(R.array.string_array_titles)[index];
+            //display the title in textview
             title.setText(topic_name);
 
+            //find the textview for content(topic description)
+            //get the content string from the array based on the tag number (index)
             TextView description = (TextView)findViewById(R.id.detail_desc);
             String desc = getResources().getStringArray(R.array.string_array_content)[index];
+            //display the content in textview
             description.setText(desc);
 
         }
--- app/src/main/java/com/example/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -10,18 +10,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //activtaes the deisplay of the detail activity when a topic is selected
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    //activtaes the display of the detail activity when a topic is selected
     public void Display(View v) {
         //Toast.makeText(this,"message",Toast.LENGTH_SHORT).show();
         ImageButton button = (ImageButton)v;
+        //tests: ignore
         //System.out.println(button.getTag().toString().toUpperCase());
         //Toast.makeText(this,button.getTag().toString(),Toast.LENGTH_SHORT).show();
+        //creates an intent directing to the detailactivity class
         Intent intent = new Intent(this, DetailActivity.class);
+        //passes the tag (io.e. identifer) of the given imageview as an argument (determines the topic selected) and activates
         intent.putExtra("topic", Integer.parseInt(button.getTag().toString()));
         startActivity(intent);
 
